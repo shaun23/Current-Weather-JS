d50e78eb1c58f93f812c6682f760974ce15c389d
Shaun23 <shaun23@hawaii.edu>
2015-10-01 21:43:00 -1000

    Added css and created table
    Moved all css into a separate file.
    Changed to add all contents into a variable and appending once.
    Created and put all weather info into a table.

--- weather.js
+++ weather.js
@@ -1,5 +1,5 @@
 var doEverything = function() {
-$(document).ready(function(){
+   $(document).ready(function(){
       $("#update").click(function() {
          $.getJSON("http://api.openweathermap.org/data/2.5/weather?zip=" + $("#zip").val(), function(result) {
             // check if data returned is valid, returns undefined if failed.
@@ -9,21 +9,28 @@ $(document).ready(function(){
                $("#stuff").append("Error: Invalid Zip Code");
             }
             else {
-               $("#stuff").append("<img src=\"http://openweathermap.org/img/w/" + result.weather[0].icon +".png\"><br>");
-               $("#stuff").append("Location: "+ result.name + "<br>");
-               $("#stuff").append("Weather: " + result.weather[0].main + "<br>");
-               $("#stuff").append("<div style=\"text-transform:capitalize\"> Cloudiness: " + result.weather[0].description + "</div>");
-               $("#stuff").append("Humidity: "+ result.main.humidity + "%<br>");
+               var contents = "<h4>" + result.name + " <img src=\"http://openweathermap.org/img/w/" + 
+                  result.weather[0].icon +".png\"></h4>";
+               contents += "Current Weather";
+               // create the table and fill its contents
+               contents += "<table class=\"table table-hover shrink\">";
+               contents += "<tr><td><b>Weather</b></td><td>" + result.weather[0].main + "</td></tr><br>";
+               contents += "<tr><td><b>Cloudiness</b></td><td><div class=\"capitalize\"> " 
+                  + result.weather[0].description + "</div></td></tr>";
+               contents += "<tr><td><b>Humidity</b></td><td>"+ result.main.humidity + "%</td></tr>";
 
                // temperatures given in Kelvins, convert to Fahrenheit 
                var temp = 9/5 * (result.main.temp - 273) + 32;
-               $("#stuff").append("Temperature: "+ temp.toFixed(0) + "°F<br>");
+               contents += "<tr><td><b>Temperature</b></td><td>"+ temp.toFixed(0) + "°F</td></tr>";
                temp = 9/5 * (result.main.temp_min - 273) + 32;
-               $("#stuff").append("Minimum Temperature: "+ temp.toFixed(0) + "°F<br>");
+               contents += "<tr><td><b>Minimum Temperature</b></td><td>"+ temp.toFixed(0) + "°F</td></tr>";
                temp = 9/5 * (result.main.temp_max - 273) + 32;
-               $("#stuff").append("Maximum Temperature: "+ temp.toFixed(0) + "°F<br>");
+               contents += "<tr><td><b>Maximum Temperature</b></td><td>"+ temp.toFixed(0) + "°F</td></tr>";
+
+               $("#stuff").append(contents);
             }
          });
       });
    });
 }
+
